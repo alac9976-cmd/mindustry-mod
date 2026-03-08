750025e9bce226a6141261bb3cf8f08b41975956
alac9976-cmd <alac9976@gmail.com>
2026-03-08 10:21:28 +0700

    Implement stats button and dialog display
    Add a button to display network statistics in a dialog.

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,1 +1,27 @@
+Events.on(ClientLoadEvent, e => {
 
+    const button = Vars.ui.hudGroup.button("Stats", () => {
+        showStats();
+    }).size(120, 50).get();
+
+    button.setPosition(20, 120);
+
+});
+
+function showStats(){
+
+    const dialog = new BaseDialog("Network Statistics");
+
+    dialog.cont.pane(t => {
+
+        const items = Vars.state.rules.defaultTeam.items();
+
+        items.each((item, amount) => {
+            t.add(item.localizedName + ": " + amount).row();
+        });
+
+    });
+
+    dialog.addCloseButton();
+    dialog.show();
+}
