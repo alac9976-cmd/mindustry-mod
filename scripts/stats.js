46d884d680387668e1ecfc4e71fb8bd59b16c013
alac9976-cmd <alac9976@gmail.com>
2026-03-08 10:30:14 +0700

    Update stats button and network statistics dialog
    Refactor stats button and dialog display logic.

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,27 +1,20 @@
 Events.on(ClientLoadEvent, e => {
 
-    const button = Vars.ui.hudGroup.button("Stats", () => {
-        showStats();
-    }).size(120, 50).get();
+    const button = new TextButton("Stats");
 
-    button.setPosition(20, 120);
+    button.clicked(() => {
 
-});
+        const dialog = new BaseDialog("Network Statistics");
 
-function showStats(){
+        dialog.cont.add("Mod loaded successfully").row();
 
-    const dialog = new BaseDialog("Network Statistics");
-
-    dialog.cont.pane(t => {
-
-        const items = Vars.state.rules.defaultTeam.items();
-
-        items.each((item, amount) => {
-            t.add(item.localizedName + ": " + amount).row();
-        });
+        dialog.addCloseButton();
+        dialog.show();
 
     });
 
-    dialog.addCloseButton();
-    dialog.show();
-}
+    Vars.ui.hudGroup.addChild(button);
+    button.setSize(120,50);
+    button.setPosition(20,120);
+
+});
